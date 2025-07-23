a46d89d21b2a8edf962bdc191a9b753c056ce2fa
Xcceler <operations@xcceler.com>
2025-07-23 09:57:49 +0530

    gggg adde

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -13,6 +13,7 @@ const Index = () => {
     <div className="min-h-screen">
       <HeroSection />
       <AboutSection />
+      gggggggg
       <MasterPlanSection />
       <ProjectHighlights />
       <PricingSection />
